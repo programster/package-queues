773b19efc42ce36693eb5da0d7658fef853f28c3
Programster <sdpagent@gmail.com>
2025-07-25 20:16:09 +0100

    documentation fix

--- src/CompletableTaskStack.php
+++ src/CompletableTaskStack.php
@@ -12,9 +12,9 @@ class CompletableTaskStack extends AbstractCompletableTaskQueue
 {
     /**
      * Call this method to check if the asynchronous queries have returned results, and handle
-     * them if they have. If connections free up, and there are pending queries, this will 
+     * them if they have. If connections free up, and there are pending queries, this will
      * send them off to the database.
-     * @return boolean - true if everything has been completed, false otherwise.
+     * @return void - true if everything has been completed, false otherwise.
      */
     public function run() : void
     {
